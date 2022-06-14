76757a080dbda2b64db36b6258e3788a7984e55d
shitaiJust <101973920+shitaiJust@users.noreply.github.com>
2022-06-14 13:54:21 +0500

    Add files via upload

---
+++ zadan11.js
@@ -0,0 +1,25 @@
+butt.onclick = function() {
+let val=""
+    val+=document.getElementById("firstName").value+" "
+val+=document.getElementById("lastName").value+" "
+val+=document.getElementById('username').value+" "
+val+=document.getElementById('email').value+" "
+val+=document.getElementById('adress').value+" "
+val+=document.getElementById('adress2').value+" "
+val+=document.getElementById('zip').value+" "
+val+=document.getElementById('ccname').value+" "
+val+=document.getElementById('ccnumber').value+" "
+val+=document.getElementById('ccsrok').value+" "
+val+=document.getElementById('cccvv').value+" "
+
+alert('hello');
+
+
+}
+
+
+
+
+
+
+
